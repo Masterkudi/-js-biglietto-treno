fe4d5cb744dbd3be5a3a24353fa08ad70f783884
masterkudi <cudini.andrea@gmail.com>
2023-07-01 10:40:02 +0200

    formula funzionante, esercizio completato

--- assets/js/bigliettoTreno.js
+++ assets/js/bigliettoTreno.js
@@ -1,23 +1,27 @@
 const userAge = parseInt(prompt('età del passeggero'));
 const distanza = parseInt(prompt('distanza da percorrere'));
+let pricePerKm = 0.21;
+let sconto = 0;
 
-let price = ('0,21€ * km');
+let price = pricePerKm * distanza;
+
+
 
 if (userAge < 18) {
-    console.log ('complimenti, hai uno sconto del 20%');
-} else {
-    if (userAge > 65) {
-        console.log ('complimenti, hai uno sconto del 40%');
-    } else if (userAge >= 18 && userAge <= 65) {
-        console.log ('hai il prezzo base');
-    }
+    sconto = 0.2;
+    console.log('Complimenti, hai uno sconto del 20%');
+} 
+else if (userAge >= 65) {
+    sconto = 0.4;
+    console.log('Complimenti, hai uno sconto del 40%');
+} 
+else {
+    console.log('Hai il prezzo base');
 }
 
-const risultato = userAge + distanza ( distanza * parseInt(price));
+const risultato = price - (price * sconto);
 
 const outputDiv = document.getElementById("output");
 
 outputDiv.innerHTML = risultato;
-
-
 
